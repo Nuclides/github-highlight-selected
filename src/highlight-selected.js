30ed0509ab516c0075271b2d21134d19f22da26f
csvenja <c.svenjax@gmail.com>
2014-11-05 22:17:47 -0800

    [Fix #2] Find in text node

--- src/highlight-selected.js
+++ src/highlight-selected.js
@@ -10,6 +10,13 @@ $(function () {
         var selection = $.trim(window.getSelection());
         if (selection) {
             var codeArea = $(".js-file-line-container");
+            codeArea.find(".js-file-line").contents().filter(function () {
+                return this.nodeType == Node.TEXT_NODE;
+            }).each(function () {
+                if (this != e.target && $(this).text().indexOf(selection) > -1) {
+                    $(this).replaceWith('<span>' + $(this).text() + '</span>');
+                }
+            });
             codeArea.find("span:not(:has(*))").each(function () {
                 if (this != e.target) {
                     if ($(this).text() == selection) {
@@ -17,7 +24,8 @@ $(function () {
                     }
                     else if ($(this).text().indexOf(selection) > -1) {
                         $(this).html(function(_, html) {
-                           return html.replace(selection, '<span class="ghs-partial-highlight">' + selection + '</span>');
+                            var re = new RegExp(selection, "g");
+                            return html.replace(re, '<span class="ghs-partial-highlight">' + selection + '</span>');
                         });
                     }
                 }

55695cf29be8896a025ed40e7d0d6846d8ad7149
Xhacker Liu <liu.dongyuan@gmail.com>
2014-11-06 22:57:35 -0800

    Better dist.sh
    Courtesy: https://github.com/danielribeiro/github-diff-highlight-extension/blob/master/package.sh

--- dist.sh
+++ dist.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-PACKAGED_PATH=`pwd`
-APP_NAME="github-highlight-selected"
-
-rm -rf /tmp/`basename $PACKAGED_PATH`
-cd /tmp
-cp -r $PACKAGED_PATH .
-cd `basename $PACKAGED_PATH`
-rm -rf .git
-rm dist.sh
-rm .gitignore
-rm -rf Assets
-rm .DS_Store
-cd /tmp
-zip -r $APP_NAME.zip `basename $PACKAGED_PATH`
-rm -rf $APP_NAME
-mv $APP_NAME.zip ~/Desktop
+echo Distributing the extension...
+zip -r ~/Desktop/github-highlight-selected.zip . -x '.*/*' '.*' 'Assets/*'
+echo Distribution file created at ~/Desktop/github-highlight-selected.zip

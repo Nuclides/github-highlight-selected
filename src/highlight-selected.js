1ea755dad15d0db78c963d9104a4cf661f12207f
PAEz <cdkpaez@gmail.com>
2015-01-07 20:02:36 +1030

    Fix highlighting stuff in same element
    Fixed highlighting stuff in the same element and while I was at it added
    the highlighted array that contains all the currently highlighted nodes.
    This make some things quicker and enabled me to add the ability to flick
    through highlights with Ctrl-Up/Down.

--- src/highlight-selected.js
+++ src/highlight-selected.js
@@ -2,10 +2,12 @@
 
     var fileLineContainer = '.js-file-line-container';
     var lastPage = location.href;
-    var textNodes;
+    var textNodes = [];
+    var highlighted = [];
+    var highlightedIndex;
 
     function wrapTextNodes(query) {
-        // Why did all the gawd dam text selection stuff fail, urgggggghh....atleast this worked!   http://cwestblog.com/2014/03/14/javascript-getting-all-text-nodes/
+        // Why did all the gawd dam text selection stuff fail, urgggggghh....atleast this worked!
         function getTextNodesIn(elem, opt_fnFilter) {
             if (elem) {
                 for (var nodes = elem.childNodes, i = nodes.length; i--;) {
@@ -31,6 +33,7 @@
             node.parentNode.replaceChild(span, node);
             textNodes[index] = span;
         });
+        textNodes.reverse();
     }
 
     wrapTextNodes(fileLineContainer);
@@ -51,32 +54,104 @@
     mutationObserver.observe(document.querySelector('#js-repo-pjax-container'), whatToObserve);
 
     function restore() {
-        [].forEach.call(document.querySelectorAll(".ghs-highlight"), function(el) {
-            el.classList.remove("ghs-highlight");
-        });
-        [].forEach.call(document.querySelectorAll(".ghs-partial-highlight"), function(el) {
-            el.parentNode.replaceChild(el.childNodes[0], el);
-        });
+        highlighted.forEach(function(el) {
+            if (el.classList.contains('ghs-highlight')) el.classList.remove("ghs-highlight");
+            else {
+                var parent = el.parentNode;
+                parent.replaceChild(el.childNodes[0], el);
+                parent.normalize();
+            }
+        })
+        highlighted = [];
     }
 
-    function replaceAll(str, target, replacement) {
-        return str.split(target).join(replacement);
+    function selectElement(el) {
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
     }
+
+    function splitReplace(el, str, selectionIndex) {
+        var strLength = str.length;
+        var source = el.textContent;
+        var pos = -1;
+        var lastPos = 0;
+        var result = document.createDocumentFragment();
+        var selected;
+        while ((pos = source.indexOf(str, pos + 1)) != -1) {
+            if (pos > lastPos) result.appendChild(document.createTextNode(source.substring(lastPos, pos)));
+            var highlight = document.createElement('span');
+            highlight.textContent = source.substring(pos, pos + strLength);
+            highlight.classList.add('ghs-partial-highlight');
+            if (pos === selectionIndex) {
+                selected = highlight;
+                highlightedIndex = highlighted.length;
+            }
+            result.appendChild(highlight);
+            highlighted.push(highlight);
+            lastPos = pos + strLength;
+        }
+        result.appendChild(document.createTextNode(source.substring(lastPos)));
+        el.replaceChild(result, el.childNodes[0]);
+        if (selected) selectElement(selected);
+    }
+
+    document.body.addEventListener('mousedown', function(e) {
+        if (e.which != 1 || highlighted.length == 0) return;
+        restore();
+    });
 
     document.body.addEventListener('mouseup', function(e) {
-        restore();
-        var selection = window.getSelection().toString().trim();
+        if (e.which != 1) return;
+        var selection = window.getSelection();
+        var selected = selection.toString().trim();
 
-        if (selection) {
+        if (selected) {
             textNodes.forEach(function(el) {
-                if (el != e.target) {
-                    if (el.textContent == selection) {
-                        el.classList.add("ghs-highlight");
-                    } else if (el.textContent.indexOf(selection) > -1) {
-                        el.innerHTML = replaceAll(el.innerHTML, selection, '<span class="ghs-partial-highlight">' + selection + '</span>');
-                    }
-                }
+                if (el.textContent == selected) {
+                    el.classList.add("ghs-highlight");
+                    if (el == e.target) highlightedIndex = highlighted.length;
+                    highlighted.push(el);
+                } else if (el.textContent.indexOf(selected) > -1) {
+                    if (el != e.target) splitReplace(el, selected);
+                    else splitReplace(el, selected, Math.min(selection.anchorOffset, selection.focusOffset));
+                };
+
             });
         }
     });
+
+    window.addEventListener('keydown', function(e) {
+
+        if (!highlighted.length > 0 || !e.ctrlKey) return;
+
+        if (e.keyCode == 38) { // down key
+            highlightedIndex--;
+            if (highlightedIndex < 0) highlightedIndex = highlighted.length - 1;
+
+        } else if (e.keyCode == 40) { // up key
+            highlightedIndex++;
+            if (highlightedIndex >= highlighted.length) highlightedIndex = 0;
+
+        } else return;
+
+        selectElement(highlighted[highlightedIndex]);
+        elShow(highlighted[highlightedIndex]);
+        e.preventDefault()
+        return false;
+    });
+
+    function elShow(el) {
+        var rect = el.getBoundingClientRect();
+        if (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight)) el.scrollIntoView(false);
+        else if (rect.top <= 0) el.scrollIntoView(true);
+    }
+
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".ghs-highlight, .ghs-partial-highlight { border: 1px solid rgba(255, 181, 21, .6); background-color: rgba(255, 181, 21, .3);}";
+    document.body.appendChild(css);
+
 })();

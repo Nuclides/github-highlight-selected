078a694107a33b61584e55fa2d9a1edea1765d1a
PAEz <cdkpaez@gmail.com>
2015-01-07 19:52:21 +1030

    Check for page updating
    Used a mutation observer to monitor the source section for updates.
    Sometimes this way is overkill and you could just do something like poll
    the breadcrumbs for changes, but this is firing at just the right time
    so sticking with it.
    Also had to change wrapTextNodes but for the life of me cant remember
    why ;)

--- src/highlight-selected.js
+++ src/highlight-selected.js
@@ -1,10 +1,61 @@
-$(function () {
+$(function() {
     var codeArea = $(".js-file-line-container");
-    codeArea.find(".js-file-line").contents().filter(function () {
-        return this.nodeType == Node.TEXT_NODE;
-    }).each(function () {
-        $(this).replaceWith('<span>' + $(this).text() + '</span>');
+    var fileLineContainer='.js-file-line-container';
+    var lastPage = location.href;
+    //     function wrapTextNodes(el){
+    //         console.log('wrapping',el)
+    //         el.find(".js-file-line").contents().filter(function () {
+    //             return this.nodeType == Node.TEXT_NODE;
+    //         }).each(function () {
+    //             $(this).replaceWith('<span>' + $(this).text() + '</span>');
+    //         });}
+
+
+    function wrapTextNodes(query) {
+        // Why did all the gawd dam text selection stuff fail, urgggggghh....atleast this worked!
+        function getTextNodesIn(elem, opt_fnFilter) {
+            var textNodes = [];
+            if (elem) {
+                for (var nodes = elem.childNodes, i = nodes.length; i--;) {
+                    var node = nodes[i],
+                        nodeType = node.nodeType;
+                    if (nodeType == 3) {
+                        if (!opt_fnFilter || opt_fnFilter(node, elem)) {
+                            textNodes.push(node);
+                        }
+                    } else if (nodeType == 1 || nodeType == 9 || nodeType == 11) {
+                        textNodes = textNodes.concat(getTextNodesIn(node, opt_fnFilter));
+                    }
+                }
+            }
+            return textNodes;
+        }
+        var el = document.querySelector(query);
+        if (!el) return;
+        var results = getTextNodesIn(el);
+        results.forEach(function(node) {
+            var span = document.createElement('span');
+            span.textContent = node.nodeValue;
+            if (node.parentNode) node.parentNode.replaceChild(span, node);
+        })
+    }
+
+    wrapTextNodes(fileLineContainer);
+
+    // watch for page updating...
+    var whatToObserve = {
+        childList: true,
+        attributes: false,
+        subtree: true,
+        attributeOldValue: false /*, attributeFilter: []*/
+    };
+    var mutationObserver = new MutationObserver(function(mutationRecords) {
+        if (location.href != lastPage) {
+            lastPage = location.href;
+            wrapTextNodes(fileLineContainer);
+        }
     });
+    mutationObserver.observe(document.querySelector('.file-navigation .breadcrumb'), whatToObserve);
 
     function restore() {
         $(".ghs-highlight").removeClass("ghs-highlight");
@@ -15,13 +66,13 @@ $(function () {
         return str.split(target).join(replacement);
     }
 
-    $("body").mouseup(function (e) {
+    $("body").mouseup(function(e) {
         restore();
-
+        var codeArea = $(fileLineContainer); // think the page updating killed this, this fixs that
         var selection = $.trim(window.getSelection());
 
         if (selection) {
-            codeArea.find("span:not(:has(*))").each(function () {
+            codeArea.find("span:not(:has(*))").each(function() {
                 if (this != e.target) {
                     if ($(this).text() == selection) {
                         $(this).addClass("ghs-highlight");

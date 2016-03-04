18acbec28e5f1169e155d1381e1d3cbdd66302a8
柳东原 LIU Dongyuan <liu.dongyuan@gmail.com>
2016-03-03 22:10:37 -0800

    [On #9] Support PRs partially
    When you expand the code in PRs, it doesn’t work for the expanded code.

--- src/highlight-selected.js
+++ src/highlight-selected.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function() {
-    var fileLineContainer = '.js-file-line-container';
+    var fileLineContainer = '.blob-wrapper';
     var lastPage = location.href;
     var textNodes = [];
     var highlighted = [];
